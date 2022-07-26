eae7d34d2a936dea3c745d47855f0f6fdc9e9f7e
elma <945495974@gmail.com>
2022-07-26 17:59:06 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+//18
+// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+ 
+//18
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+ 
+//17
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+ 
+ 
+reportWebVitals();
