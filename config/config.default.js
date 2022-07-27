797a8e23bb3cb6e5fe1fe1ed23f3efbc8c89a7ce
顶峰云雪 <pinus0716@163.com>
2022-07-27 20:27:03 +0800

    设置cors

--- config/config.default.js
+++ config/config.default.js
@@ -37,6 +37,7 @@ module.exports = appInfo => {
 		csrf: {
 			enable: false,
 		},
+		domainWhiteList: ['*'], // 跨域配置
 	}
 
 	config.bodyParser = {
--- config/plugin.js
+++ config/plugin.js
@@ -10,4 +10,9 @@ module.exports = {
 		enable: true,
 		package: 'egg-sequelize',
 	},
+
+	cors: {
+		enable: true,
+		package: 'egg-cors',
+	},
 }
