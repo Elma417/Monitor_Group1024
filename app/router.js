d20a1fb9e547ebe3b4dccb5414371434e8c3a079
顶峰云雪 <pinus0716@163.com>
2022-07-27 15:58:20 +0800

    开启测试的路由，测试线上运行

--- app/router.js
+++ app/router.js
@@ -5,7 +5,7 @@
  */
 module.exports = app => {
 	const { router, controller } = app
-	// router.get('/', controller.home.index)
+	router.get('/', controller.home.index)
 
 	// 上传日志
 	router.all('/logstore/track', controller.logstore.track)
